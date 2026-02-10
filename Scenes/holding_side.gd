8e0bfdcdc9042606bf77cb2a7cfa1cb99a9e42e8
Gammos <gammos@gmx.de>
2026-02-10 02:10:59 +0100

    latest changes

--- Scenes/holding_side.gd
+++ Scenes/holding_side.gd
@@ -8,8 +8,9 @@ func take(node):
 		call_deferred("add_child", node)
 		node.position = Vector3.ZERO
 		node.rotation_degrees = Vector3.ZERO
-		node.collision(false)
-		node.freeze = true
+		if node is RigidBody3D:
+			node.collision(false)
+			node.freeze = true
 		holding = node
 
 func drop():
@@ -18,8 +19,9 @@ func drop():
 		remove_child(holding)
 		WORLD.get_node("dropped_items").add_child(holding)
 		holding.set_global_transform($drop_position.get_global_transform())
-		holding.collision(true)
-		holding.linear_velocity = Vector3.ZERO
-		holding.angular_velocity = Vector3.ZERO
-		holding.freeze = false
+		if holding is RigidBody3D:
+			holding.collision(true)
+			holding.linear_velocity = Vector3.ZERO
+			holding.angular_velocity = Vector3.ZERO
+			holding.freeze = false
 		holding = null
